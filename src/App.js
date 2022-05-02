37122fd3b5eac4a40deff04bde0aeb052c205867
Calum <calum.rogers@ntlworld.com>
2022-05-02 11:34:21 +0100

    third commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import Input from './components/Input';
 import './App.css';
+import TodoItem from './components/TodoItem';
+
+const todoList =[{
+  item:'todo',
+  done: false,
+  id: 695426487923
+},{
+  item:'todo2',
+  done: true,
+  id: 85838
+
+}]
+
 
 function App() {
   return (
     <div className="app">
-      {/* todo list */}
-      {/* check box */}
+      <div className='app_container'>
+        <div className='app_todoContainer'>
+          {
+            todoList.map(item=>(
+              <TodoItem 
+              name={item.item}
+              done={item.done}
+              id={item.id}/>
+            ))
+          }
+        </div>
+      </div>
       <Input/>
     </div>
   );
